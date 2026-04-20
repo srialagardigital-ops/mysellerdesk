8814545604b15845b362ff895529159ac855c5eb
srialagardigital-ops <srialagardigital@gmail.com>
2026-04-20 20:21:48 +0530

    Add files via upload

---
+++ dashboard-page-config.js
@@ -0,0 +1,7 @@
+window.SELLERDESK_CONFIG = {
+  SITE_URL: "https://mysellerdesk.in",
+  APP_URL: "https://app.mysellerdesk.in",
+  SUPABASE_URL: "https://zkafglojonspzlsfxcdh.supabase.co",
+  SUPABASE_ANON_KEY: "PASTE_YOUR_REAL_SUPABASE_ANON_KEY_HERE",
+  DASHBOARD_PATH: "/index.html"
+};
